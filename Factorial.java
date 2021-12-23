8cbe6567f65a62067a8d0dcbdf73fe1945ef2996
Preeti Srivastava <95173525+SrivastavaPreeti@users.noreply.github.com>
2021-12-23 17:44:27 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+	int i=1;
+	int sum=0;
+	int n=8;
+	
+	for(i=1;i<=n;i++)
+	
+	{
+		sum = sum+i;
+	}
+		System.out.println(sum);
+	}
+		
+}
+	
+	
