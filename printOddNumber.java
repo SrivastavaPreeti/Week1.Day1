40b545ea149bb71dc6069367232e16d8c9af4dec
Preeti Srivastava <95173525+SrivastavaPreeti@users.noreply.github.com>
2021-12-25 19:44:57 +0530

    Add files via upload

---
+++ printOddNumber.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class findOddNumber {
+
+	public static void main(String[] args) {
+		int i = 1;
+		
+	for (i=1;i<=20;i++)
+	
+	if (i%2==1)
+	
+	{
+		System.out.println(i);
+	}
+	}
+
+}
